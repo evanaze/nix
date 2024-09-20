60f5ebd2212c6f9c2ead95217337ac516640239d
Evan Azevedo <me@evanazevedo.com>
2024-09-20 11:13:22 -0600

    main install with hm

--- home/shared.nix
+++ home/shared.nix
@@ -9,6 +9,7 @@
 
   home.packages = with pkgs; [
     cargo
+    devenv
     go
     hugo
     nodejs
--- hosts/shared.nix
+++ hosts/shared.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     cron
-    devenv
     git
     htop
     meslo-lgs-nf
