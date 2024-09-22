7f9797d68c9472a780cdb6730c8bdb7895d6e3e5
Evan Azevedo <me@evanazevedo.com>
2024-09-22 14:16:07 -0600

    main avahi for name resolution

--- hosts/shared.nix
+++ hosts/shared.nix
@@ -12,4 +12,16 @@
     unzip
     wget
   ];
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    ipv4 = true;
+    ipv6 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
+  };
 }
