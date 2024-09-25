9445e0069c90cfc8ed83a97edddfcfeaa3932304
Evan Azevedo <me@evanazevedo.com>
2024-09-25 08:22:07 -0600

    main changes

--- hosts/rpi/default.nix
+++ hosts/rpi/default.nix
@@ -71,16 +71,6 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     firefox
-  #     tree
-  #   ];
-  # };
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
