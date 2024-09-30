b6c6bc7d63118022f49b0fe29b1a618aff28262e
Evan Azevedo <me@evanazevedo.com>
2024-09-30 08:13:59 -0600

    main letting aarch

--- home/nixvim/flake.nix
+++ home/nixvim/flake.nix
@@ -16,7 +16,7 @@
     ...
   } @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "x86_64-darwin"];
+      systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux"];
 
       perSystem = {
         system,
