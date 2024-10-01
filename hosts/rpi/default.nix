dc7e34fcdd407174db0316388e8b82db83a572c2
Evan Azevedo <me@evanazevedo.com>
2024-10-01 11:28:02 -0600

    main giving static ip

--- hosts/rpi/default.nix
+++ hosts/rpi/default.nix
@@ -35,6 +35,22 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   networking.firewall.enable = true;
+
+  # Set static IP over ethernet
+  networking = {
+    interfaces.end0 = {
+      ipv4.addresses = [
+        {
+          address = "192.168.50.150";
+          prefixLength = 24;
+        }
+      ];
+    };
+    defaultGateway = {
+      address = "192.168.50.1";
+      interface = "end0";
+    };
+  };
   networking.firewall.allowedTCPPorts = [22];
   services.openssh = {
     enable = true;
