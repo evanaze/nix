f82ef5c837ebc577df082dbecf0f9fa7a68dbaed
Evan Azevedo <me@evanazevedo.com>
2024-10-01 10:56:15 -0600

    main got blocky working

--- hosts/rpi/blocky.nix
+++ hosts/rpi/blocky.nix
@@ -1,8 +1,32 @@
 {...}: {
-  blocky = {
+  services.blocky = {
     enable = true;
+    settings = {
+      ports.dns = 53;
+      upstreams.groups.default = [
+        "https://one.one.one.one/dns-query"
+      ];
+      bootstrapDns = {
+        upstream = "https://one.one.one.one/dns-query";
+        ips = ["1.1.1.1" "1.0.0.1"];
+      };
+      blocking = {
+        denylists = {
+          #Adblocking
+          ads = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"];
+        };
+        clientGroupsBlock = {
+          default = ["ads"];
+        };
+      };
+      caching = {
+        minTime = "5m";
+        maxTime = "30m";
+        prefetching = true;
+      };
+    };
   };
 
-  firewall.allowedTCPPorts = [53];
-  firewall.allowedUDPPorts = [53];
+  networking.firewall.allowedTCPPorts = [53];
+  networking.firewall.allowedUDPPorts = [53];
 }
