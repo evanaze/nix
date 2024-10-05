8f558fbd3cc037877e1c5e9d107130821f7efb18
Evan Azevedo <me@evanazevedo.com>
2024-10-04 19:32:38 -0600

    main simplifying stuff

--- hosts/rpi/default.nix
+++ hosts/rpi/default.nix
@@ -22,8 +22,6 @@
     initrd.systemd.tpm2.enable = false;
   };
 
-  networking.hostName = "hs";
-
   nix = {
     settings.experimental-features = ["nix-command" "flakes"];
     nixPath = [
@@ -35,10 +33,10 @@
 
   i18n.defaultLocale = "en_US.UTF-8";
 
-  networking.firewall.enable = true;
-
   # Set static IP over ethernet
   networking = {
+    hostname = "hs";
+    firewall.enable = true;
     interfaces.end0 = {
       ipv4.addresses = [
         {
@@ -65,57 +63,5 @@
       PermitRootLogin = "no";
     };
   };
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.11"; # Did you read the comment?
 }
