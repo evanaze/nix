403ab4c073ce372a1fbef36fb0926c33a7847487
Evan Azevedo <me@evanazevedo.com>
2024-10-12 10:24:07 -0600

    main trying friendly-snip

--- home/nixvim/config/plugins/cmp/cmp.nix
+++ home/nixvim/config/plugins/cmp/cmp.nix
@@ -54,6 +54,7 @@
   };
   extraConfigLua = ''
         luasnip = require("luasnip")
+        require("luasnip.loaders.from_vscode").lazy_load()
         kind_icons = {
           Text = "󰊄",
           Method = " ",
