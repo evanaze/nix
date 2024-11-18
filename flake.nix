11c5e5cc27e240b331d64d1eb0a04e15c608abc0
Evan Azevedo <me@evanazevdo.com>
2024-11-17 17:21:44 -0700

    main update homemanager

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim.url = "github:evanaze/nixvim-conf";
