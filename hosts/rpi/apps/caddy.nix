2be0c419d4d06b2046617058ebb4acc28fc8753a
Evan Azevedo <me@evanazevedo.com>
2025-01-20 10:05:39 -0700

    remove port

--- hosts/rpi/apps/caddy.nix
+++ hosts/rpi/apps/caddy.nix
@@ -4,7 +4,7 @@ in {
   services.caddy = {
     enable = true;
     email = "me@evanazevedo.com";
-    virtualHosts."${domain}:2019".extraConfig = ''
+    virtualHosts."${domain}".extraConfig = ''
       root * /var/www/${domain}
       file_server
     '';
