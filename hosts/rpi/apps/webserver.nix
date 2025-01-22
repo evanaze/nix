a99c91ae41a599ed6f61186f4d5b6cf483178dd7
Evan Azevedo <me@evanazevedo.com>
2025-01-22 10:07:23 -0700

    cloudflared

--- hosts/rpi/apps/webserver.nix
+++ hosts/rpi/apps/webserver.nix
@@ -14,5 +14,17 @@ in {
 
   services.cloudflared = {
     enable = true;
+    tunnels = {
+      "3c99cee8-ecb7-4274-8b6b-b50df55bc5c5" = {
+        credentialsFile = "/home/evanaze/.cloudflared/3c99cee8-ecb7-4274-8b6b-b50df55bc5c5.json";
+        ingress = {
+          "*.${domain}" = {
+            service = "http://localhost:80";
+            path = "/*.(jpg|png|css|js)";
+          };
+        };
+        default = "http_status:404";
+      };
+    };
   };
 }
