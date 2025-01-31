6c0b5f51b064764270b17813c17d6b220ab27bfc
Evan Azevedo <me@evanazevedo.com>
2025-01-31 09:12:11 -0700

    chatbot service

--- hosts/desktop/apps/ai.nix
+++ hosts/desktop/apps/ai.nix
@@ -19,16 +19,16 @@
     loadModels = ["deepseek-r1:14b"];
   };
 
-  # systemd.services.tailscale-serve-open-webui = {
-  #   after = ["tailscaled.service"];
-  #   wants = ["tailscaled.service"];
-  #   wantedBy = ["multi-user.target"];
-  #   description = "Using Tailscale Serve to publish my AI Chatbot";
-  #   serviceConfig = {
-  #     Type = "exec";
-  #   };
-  #   script = ''
-  #     ${pkgs.tailscale} serve 8080
-  #   '';
-  # };
+  systemd.services.tailscale-serve-open-webui = {
+    after = ["tailscaled.service"];
+    wants = ["tailscaled.service"];
+    wantedBy = ["multi-user.target"];
+    description = "Using Tailscale Serve to publish my AI Chatbot";
+    serviceConfig = {
+      Type = "exec";
+    };
+    script = ''
+      ${pkgs.tailscale}/bin/tailscale serve 8080
+    '';
+  };
 }
