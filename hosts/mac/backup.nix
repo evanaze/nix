1760e8bef34c2e4d376360aad7ce56f81181321b
Evan Azevedo <me@evanazevdo.com>
2025-02-09 10:50:29 -0700

    try more folders

--- hosts/mac/backup.nix
+++ hosts/mac/backup.nix
@@ -2,14 +2,33 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  syncScript = ''
+    #!/bin/bash
+
+    # List of folders to sync
+    folders=("Documents" "Music" "Movies")
+
+    # Loop through each folder and run the rclone sync command
+    for folder in "$\{folders[@]\}"; do
+        echo "Syncing folder: $folder"
+
+        ${lib.getExe pkgs.rclone} sync "/Users/evanaze/$folder" \
+            "rpi:kb9n9h7fxq@privaterelay.appleid.com/hs/backup/mac/current/$folder" \
+            --backup-dir="rpi:kb9n9h7fxq@privaterelay.appleid.com/hs/backup/mac/archive/$(date -I)/$folder" \
+            --config="/Users/evanaze/.config/rclone/rclone.conf"
+
+        echo "Finished syncing $folder"
+    done
+  '';
+in {
   environment.systemPackages = with pkgs; [
     rclone
   ];
 
   # Backups using RClone and Tailscale Taildrive
   launchd.agents.rclone = {
-    command = "${lib.getExe pkgs.rclone} sync /Users/evanaze/Documents rpi:kb9n9h7fxq@privaterelay.appleid.com/hs/backup/mac/current/Documents --backup-dir=rpi:kb9n9h7fxq@privaterelay.appleid.com/hs/backup/mac/archive/Documents`date -I` --config=/Users/evanaze/.config/rclone/rclone.conf";
+    script = syncScript;
     serviceConfig = {
       KeepAlive = false;
       ProcessType = "Background";
