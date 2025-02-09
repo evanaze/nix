ade041a6ea247251b599942ce3a400d95ef6a92c
Evan Azevedo <me@evanazevdo.com>
2025-02-09 10:30:59 -0700

    get exe

--- hosts/mac/backup.nix
+++ hosts/mac/backup.nix
@@ -1,11 +1,15 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   environment.systemPackages = with pkgs; [
     rclone
   ];
 
   # Backups using RClone and Tailscale Taildrive
   launchd.agents.rclone = {
-    command = "${pkgs.rclone} sync /Users/evanaze/Documents rpi:kb9n9h7fxq@privaterelay.appleid.com/hs/backup/mac/current/Documents --backup-dir=rpi:kb9n9h7fxq@privaterelay.appleid.com/hs/backup/mac/archive/Documents`date -I` --config=/Users/evanaze/.config/rclone/rclone.conf";
+    command = "${lib.getExe} ${pkgs.rclone} sync /Users/evanaze/Documents rpi:kb9n9h7fxq@privaterelay.appleid.com/hs/backup/mac/current/Documents --backup-dir=rpi:kb9n9h7fxq@privaterelay.appleid.com/hs/backup/mac/archive/Documents`date -I` --config=/Users/evanaze/.config/rclone/rclone.conf";
     serviceConfig = {
       KeepAlive = false;
       ProcessType = "Background";
