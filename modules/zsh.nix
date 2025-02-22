8da4ad2335be995f4fec9e4cc581daaee618cf70
Evan Azevedo <me@evanazevedo.com>
2025-02-22 09:14:48 -0700

    update nixvim command

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -52,6 +52,7 @@
           sshpi = "ssh evanaze@hs.spitz-pickerel.ts.net";
           sshdt = "ssh evanaze@father.spitz-pickerel.ts.net";
           update = "pushd $HOME/.config/nix && nix flake update && popd";
+          updnvim = "pushd $HOME/.config/nix && nix flake update nixvim && popd";
           npush = "pushd $HOME/.config/nix && epush $@ && popd";
         };
       };
