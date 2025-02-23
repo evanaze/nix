98c987ea253c8819f4f32ca4b91719d9d65fa826
Evan Azevedo <me@evanazevdo.com>
2025-02-23 10:48:36 -0700

    fixing mac error

--- hosts/mac/default.nix
+++ hosts/mac/default.nix
@@ -14,9 +14,6 @@
     shell = pkgs.zsh;
   };
 
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
-
   system.stateVersion = 4;
 
   # The platform the configuration will be used on.
