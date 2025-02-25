cdabdf064002f1d1badcd684100e8151694b1f4f
Evan Azevedo <me@evanazevedo.com>
2025-02-24 20:39:11 -0700

    enabling opengl

--- modules/slippi.nix
+++ modules/slippi.nix
@@ -7,6 +7,8 @@
     };
   };
 
+  hardware.opengl.enable = true;
+
   services.udev.extraRules = ''
     SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0337", MODE="0666"
   '';
