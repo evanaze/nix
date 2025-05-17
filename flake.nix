0083b3eb8119f63c0dc869e04a586c7902996adb
Evan Azevedo <me@evanazevedo.com>
2025-05-17 07:48:17 -0600

    downgrade to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -53,7 +53,7 @@
             home-manager.users.${username} = {
               imports = [
                 ./home/desktop.nix
-                slippi.homeManagerModules.default 
+                slippi.homeManagerModules.default
               ];
             };
           }
