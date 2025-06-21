9e35d43106df8694621c3910446ac54eff05fd86
Evan Azevedo <me@evanazevedo.com>
2025-06-21 08:50:59 -0600

    adding devstral

--- hosts/desktop/apps/ai/ollama.nix
+++ hosts/desktop/apps/ai/ollama.nix
@@ -8,6 +8,6 @@
     port = 11434;
     package = pkgs.ollama-cuda;
     acceleration = "cuda";
-    loadModels = ["qwen2.5:14b" "qwen2.5-coder:14b"];
+    loadModels = ["qwen2.5:14b" "qwen2.5-coder:14b" "devstral"];
   };
 }
