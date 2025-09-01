17cc9a53e715b3b3803020d0b9f33e769166632a
Evan Azevedo <me@evanazevedo.com>
2025-08-31 19:58:48 -0600

    framework kernel patch

--- hosts/framework/default.nix
+++ hosts/framework/default.nix
@@ -33,6 +33,7 @@
 
   # Enable bios updates
   services.fwupd.enable = true;
+  hardware.framework.enableKmod = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
