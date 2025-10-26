adb4388cd142acdaf3e0865a5abcb49ec6bd0fa1
Evan Azevedo <me@evanazevedo.com>
2025-10-25 18:12:49 -0600

    fix syntax

--- hosts/shared/pc/gnome-keyring.nix
+++ hosts/shared/pc/gnome-keyring.nix
@@ -3,6 +3,6 @@
   security.pam.services.gdm.enableGnomeKeyring = true;
   environment = {
     variables.XDG_RUNTIME_DIR = "/run/user/$UID"; # set the runtime directory
-    environment.systemPackages = [pkgs.libsecret]; # libsecret API
+    systemPackages = [pkgs.libsecret]; # libsecret API
   };
 }
