a23c379bdf1b435fe96cb23d6f978a4d3c11aae2
Evan Azevedo <me@evanazevedo.com>
2026-01-18 17:47:40 -0700

    small change

--- home/shared/zsh.nix
+++ home/shared/zsh.nix
@@ -57,8 +57,8 @@
         dup = "devenv up";
         showsvcs = "systemctl --type=service --state=running";
         svc-stat = "journalctl -xe -u $1";
-        sshpi = "ssh evanaze@hs.spitz-pickerel.ts.net";
-        sshdt = "ssh evanaze@father.spitz-pickerel.ts.net";
+        sshpi = "ssh evanaze@mercury.spitz-pickerel.ts.net";
+        sshdt = "ssh evanaze@earth.spitz-pickerel.ts.net";
         update = "pushd $HOME/.config/nix && git pull && nix flake update && epush updating flake inputs && popd";
         unpack = "fd . $1/ -x mv {} .";
         updnvim = "pushd $HOME/.config/nix && nix flake update nixvim && epush updating nixvim && popd";
