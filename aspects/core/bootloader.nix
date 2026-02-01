eb9bb88b89a80bc354b69d86253eddea01323dfa
Evan Azevedo <me@evanazevedo.com>
2026-02-01 16:29:02 -0700

    adding grub bootloader

--- aspects/core/bootloader.nix
+++ aspects/core/bootloader.nix
@@ -4,5 +4,9 @@
   boot.loader = {
     systemd-boot.enable = lib.mkDefault true;
     efi.canTouchEfiVariables = lib.mkDefault true;
+    grub = {
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+    };
   };
 }
