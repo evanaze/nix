7e1cc6d2bf530c8a53d7f04bbc9a21add0e71ad3
Evan Azevedo <me@evanazevdo.com>
2026-02-05 08:22:49 -0700

    adding zfs again

--- aspects/hardware/jupiter/zfs.nix
+++ aspects/hardware/jupiter/zfs.nix
@@ -1,5 +1,14 @@
-{...}: {
+{pkgs, ...}: {
   boot.supportedFilesystems = ["zfs"];
   boot.zfs.forceImportRoot = false;
   networking.hostId = "e8a69b01"; # head -c4 /dev/urandom | od -A none -t x4
+  # zpool create -f -o ashift=12 atime=off -m /mnt/eye eye \
+  #   raidz \
+  #   ata-WDC_WD120EAGZ-00CRJB0_WD-B00N465D \
+  #   ata-WDC_WD120EAGZ-00CRJB0_WD-B00N73PD \
+  #   ata-WDC_WD120EAGZ-00CRJB0_WD-B00NAZ1D
+
+  environment.systemPackages = with pkgs; [
+    zfs
+  ];
 }
