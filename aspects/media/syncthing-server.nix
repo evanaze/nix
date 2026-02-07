18badbec6a5ea452d4bd6c8f2a89aac568e6b448
Evan Azevedo <me@evanazevdo.com>
2026-02-07 11:17:24 -0700

    disable systemd for now

--- aspects/media/syncthing-server.nix
+++ aspects/media/syncthing-server.nix
@@ -14,20 +14,20 @@
     };
   };
 
-  systemd.services.jellyfin-tsserve = {
-    after = [
-      "tailscaled-autoconnect.service"
-      "syncthing.service"
-    ];
-    wants = [
-      "tailscaled-autoconnect.service"
-      "syncthing.service"
-    ];
-    wantedBy = ["multi-user.target"];
-    description = "Using Tailscale Serve to publish Syncthing";
-    serviceConfig = {
-      Type = "exec";
-    };
-    script = "${lib.getExe pkgs.tailscale} serve --bg https 443 --set-path /media 8384";
-  };
+  # systemd.services.syncthing-tsserve = {
+  #   after = [
+  #     "tailscaled-autoconnect.service"
+  #     "syncthing.service"
+  #   ];
+  #   wants = [
+  #     "tailscaled-autoconnect.service"
+  #     "syncthing.service"
+  #   ];
+  #   wantedBy = ["multi-user.target"];
+  #   description = "Using Tailscale Serve to publish Syncthing";
+  #   serviceConfig = {
+  #     Type = "exec";
+  #   };
+  #   script = "${lib.getExe pkgs.tailscale} serve --service=svc:docs --https=443 8384";
+  # };
 }
