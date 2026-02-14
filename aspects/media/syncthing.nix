d648b7ba69a1b3419afef96fe794528d5dc01432
Evan Azevedo <me@evanazevedo.com>
2026-02-14 10:56:36 -0700

    setting user

--- aspects/media/syncthing.nix
+++ aspects/media/syncthing.nix
@@ -6,6 +6,7 @@
 }: {
   services.syncthing = {
     enable = true;
+    user = username;
     openDefaultPorts = false;
     guiPasswordFile = "/run/secrets/admin-pass";
     guiAddress = "127.0.0.1:8384";
