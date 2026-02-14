c5fa1e81719e4855b163e47205984e27fcb5d003
Evan Azevedo <me@evanazevedo.com>
2026-02-14 11:22:05 -0700

    hostname key and certs

--- aspects/media/syncthing.nix
+++ aspects/media/syncthing.nix
@@ -1,6 +1,5 @@
 {
-  lib,
-  pkgs,
+  hostname,
   username,
   ...
 }: {
@@ -8,8 +7,8 @@
     enable = true;
     user = username;
     dataDir = "/home/${username}";
-    # key = "/run/secrets/syncthing-key-pem";
-    # cert = "/run/secrets/syncthing-cert-pem";
+    key = "/run/secrets/syncthing-${hostname}-key-pem";
+    cert = "/run/secrets/syncthing-${hostname}-cert-pem";
     openDefaultPorts = false;
     guiPasswordFile = "/run/secrets/admin-pass";
     guiAddress = "127.0.0.1:8384";
