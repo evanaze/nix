21d656e5160d14be890b812ed28cc0eaa2dfdb1b
Evan Azevedo <me@evanazevedo.com>
2026-02-16 14:27:53 -0700

    adding ts service to immich

--- aspects/media/immich.nix
+++ aspects/media/immich.nix
@@ -1,4 +1,9 @@
-{username, ...}: {
+{
+  lib,
+  pkgs,
+  username,
+  ...
+}: {
   users.groups.media = {};
 
   users.users.${username}.extraGroups = ["media"];
@@ -8,5 +13,22 @@
     group = "media";
     port = 2283;
     mediaLocation = "/mnt/eye/pictures";
+  };
+
+  systemd.services.immich-tsserve = {
+    after = [
+      "tailscaled-autoconnect.service"
+      "immich-server.service"
+    ];
+    wants = [
+      "tailscaled-autoconnect.service"
+      "immich-server.service"
+    ];
+    wantedBy = ["multi-user.target"];
+    description = "Using Tailscale Serve to publish Immich";
+    serviceConfig = {
+      Type = "exec";
+    };
+    script = "${lib.getExe pkgs.tailscale} serve --service=svc:photos --https=443 2283";
   };
 }
