07ce5fbb90f4a708bc7ad5b035b50e1bb9d576cf
Evan Azevedo <me@evanazevedo.com>
2026-02-16 12:04:33 -0700

    adding mars to syncthing

--- aspects/media/syncthing.nix
+++ aspects/media/syncthing.nix
@@ -21,6 +21,10 @@
           addresses = ["tcp://100.99.77.56:22000"];
           id = "7Y3UBW3-H2FYEKH-44BQUYC-TJXCAC7-WSMGVEP-GZM5DXK-QV5XPKG-ZBG55AY";
         };
+        "mars" = {
+          addresses = ["tcp://100.96.108.85:22000"];
+          id = "WQRGUVJ-TQOHTMX-3A4BCPU-KM4U25U-MRETURY-R377JJA-RUPYHAD-HTXWYQV";
+        };
         "jupiter" = {
           addresses = ["tcp://100.114.214.80:22000"];
           id = "HUSB4BP-ISO6UAK-2KL3PND-VQX3OE7-F5ZRFPR-IX7K3UV-Q3UCMFW-34C2XAV";
