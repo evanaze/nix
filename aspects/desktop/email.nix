16502e0bcb0392659813ef61c78f09e317960b6f
Evan Azevedo <me@evanazevedo.com>
2026-02-18 12:22:56 -0700

    working on email setup

--- aspects/desktop/email.nix
+++ aspects/desktop/email.nix
@@ -1,17 +1,20 @@
-{...}: {
-  programs.thunderbird = {
-    enable = true;
-  };
+{username, ...}: {
+  home-manager.users.${username} = {pkgs, ...}: {
+    programs.thunderbird = {
+      enable = true;
+      profiles."Default".isDefault = true;
+    };
 
-  # Thunderbird email configuration
-  accounts.email.accounts = {
-    evanaze = {
-      name = "Evanaze Gmail";
-      address = "evanaze@gmail.com";
-      flavor = "gmail.com";
-      primary = true;
-      realName = "Evan Azevedo";
-      thunderbird.enable = true;
+    # Thunderbird email configuration
+    accounts.email.accounts = {
+      evanaze = {
+        # name = "Evanaze Gmail";
+        address = "evanaze@gmail.com";
+        flavor = "gmail.com";
+        primary = true;
+        realName = "Evan Azevedo";
+        thunderbird.enable = true;
+      };
     };
   };
 }
