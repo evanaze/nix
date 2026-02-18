c9dd3e63521b480ca720727eae01d1667cd92466
Evan Azevedo <me@evanazevedo.com>
2026-02-18 12:28:09 -0700

    adding first gmail

--- aspects/desktop/email.nix
+++ aspects/desktop/email.nix
@@ -7,13 +7,20 @@
 
     # Thunderbird email configuration
     accounts.email.accounts = {
-      evanaze = {
-        # name = "Evanaze Gmail";
+      "evanaze@gmail.com" = {
         address = "evanaze@gmail.com";
         flavor = "gmail.com";
         primary = true;
         realName = "Evan Azevedo";
         thunderbird.enable = true;
+        imap = {
+          host = "imap.gmail.com";
+          tls.useStartTls = true;
+        };
+        smtp = {
+          host = "smtp.gmail.com";
+          tls.useStartTls = true;
+        };
       };
     };
   };
