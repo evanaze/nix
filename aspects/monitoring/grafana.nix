0d366e20c8ce2459c14da2f7f89c1ebefe1bc339
Evan Azevedo <me@evanazevdo.com>
2026-03-03 12:21:08 -0700

    tsserve grafana

--- aspects/monitoring/grafana.nix
+++ aspects/monitoring/grafana.nix
@@ -15,21 +15,21 @@
     };
   };
 
-  # systemd.services.grafana-tsserve = {
-  #   after = [
-  #     "tailscaled-autoconnect.service"
-  #     "immich-server.service"
-  #   ];
-  #   wants = [
-  #     "tailscaled-autoconnect.service"
-  #     "immich-server.service"
-  #   ];
-  #   wantedBy = ["multi-user.target"];
-  #   description = "Using Tailscale Serve to publish Grafana";
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     RemainAfterExit = true;
-  #   };
-  #   script = "${lib.getExe pkgs.tailscale} serve --service=svc:monitoring --https=4431 http://127.0.0.1:2342";
-  # };
+  systemd.services.grafana-tsserve = {
+    after = [
+      "tailscaled-autoconnect.service"
+      "grafana.service"
+    ];
+    wants = [
+      "tailscaled-autoconnect.service"
+      "grafana.service"
+    ];
+    wantedBy = ["multi-user.target"];
+    description = "Using Tailscale Serve to publish Grafana";
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    script = "${lib.getExe pkgs.tailscale} serve --service=svc:monitoring --https=4431 http://127.0.0.1:2342";
+  };
 }
