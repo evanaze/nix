cf57f1abc153e502affffcb0977b87f568a81d94
Evan Azevedo <me@evanazevedo.com>
2026-03-03 12:01:53 -0700

    working on prometheus

--- aspects/monitoring/grafana.nix
+++ aspects/monitoring/grafana.nix
@@ -1,6 +1,30 @@
 # aspects/monitoring/grafana.nix - Grafana dashboards
-{...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   services.grafana = {
     enable = true;
+    port = 2342;
+    addr = "127.0.0.1";
   };
+
+  # systemd.services.grafana-tsserve = {
+  #   after = [
+  #     "tailscaled-autoconnect.service"
+  #     "immich-server.service"
+  #   ];
+  #   wants = [
+  #     "tailscaled-autoconnect.service"
+  #     "immich-server.service"
+  #   ];
+  #   wantedBy = ["multi-user.target"];
+  #   description = "Using Tailscale Serve to publish Grafana";
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     RemainAfterExit = true;
+  #   };
+  #   script = "${lib.getExe pkgs.tailscale} serve --service=svc:monitoring --https=4431 http://127.0.0.1:2342";
+  # };
 }
--- aspects/monitoring/prometheus.nix
+++ aspects/monitoring/prometheus.nix
@@ -1,12 +1,23 @@
 # aspects/monitoring/prometheus.nix - Prometheus monitoring
-{...}: {
+{config, ...}: {
   services.prometheus = {
-    enable = false;
-    exporters.node.enable = true;
+    enable = true;
+    port = 9001;
+    exporters = {
+      node = {
+        enable = true;
+        port = 9002;
+        enabledCollectors = ["systemd"];
+      };
+    };
 
     globalConfig.scrape_interval = "15s";
 
-    scrapeConfigs.mtrc.job_name = "node";
-    scrapeConfigs.mtrc.staticConfigs.mtrc.targets = ["localhost:9100"];
+    scrapeConfigs = [
+    {
+        job_name = "chrysalis";
+        static_configs = [{
+          targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
+        }];
   };
 }
