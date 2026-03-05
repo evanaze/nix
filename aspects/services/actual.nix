c9f77f7bbbdb8b65ea96b7e21435cd037140b8e9
Evan Azevedo <me@evanazevdo.com>
2026-03-05 10:27:10 -0700

    tsserve actual

--- aspects/services/actual.nix
+++ aspects/services/actual.nix
@@ -1,3 +1,25 @@
-{...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   services.actual.enable = true;
+
+  systemd.services.actual-tsserve = {
+    after = [
+      "tailscaled-autoconnect.service"
+      "actual.service"
+    ];
+    wants = [
+      "tailscaled-autoconnect.service"
+      "actual.service"
+    ];
+    wantedBy = ["multi-user.target"];
+    description = "Using Tailscale Serve to publish Actual";
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    script = "${lib.getExe pkgs.tailscale} serve --service=svc:budget --https=4432 http://127.0.0.1:5006";
+  };
 }
