1812ad5391e700dcae53ed797dded9f8e96f605e
Evan Azevedo <me@evanazevdo.com>
2026-03-09 10:07:21 -0600

    improving jellyfin performance

--- aspects/media/jellyfin.nix
+++ aspects/media/jellyfin.nix
@@ -17,6 +17,21 @@
     dataDir = "/mnt/eye/media";
   };
 
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      rocmPackages.clr.icd # OpenCL for AMD
+      libva-utils
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+
+  users.users.${username}.extraGroups = [
+    "video"
+    "render"
+  ];
+
   systemd.services.jellyfin-tsserve = {
     after = [
       "tailscaled-autoconnect.service"
