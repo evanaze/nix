d8e7ce49c599099e569601a27dd6ba8e44013992
Evan Azevedo <me@evanazevedo.com>
2026-04-23 20:51:48 -0600

    bluetooth

--- aspects/hardware/earth.nix
+++ aspects/hardware/earth.nix
@@ -55,4 +55,19 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        ControllerMode = "bredr"; # Fix frequent Bluetooth audio dropouts
+        Experimental = true;
+        FastConnectable = true;
+      };
+      Policy = {
+        AutoEnable = true;
+      };
+    };
+  };
 }
