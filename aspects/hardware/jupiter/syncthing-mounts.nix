636ae98aef330e044d2780725671d0dd22c323bd
Evan Azevedo <me@evanazevdo.com>
2026-04-27 21:15:01 -0600

    fix syncthing mount

--- aspects/hardware/jupiter/syncthing-mounts.nix
+++ aspects/hardware/jupiter/syncthing-mounts.nix
@@ -4,8 +4,8 @@
       what = "/mnt/eye/media/music";
       where = "/home/evanaze/Music";
       type = "none";
-      wantedBy = [ "syncthing.service" ];
-      before = [ "syncthing.service" ];
+      wantedBy = ["syncthing.service"];
+      before = ["syncthing.service"];
       mountConfig = {
         Options = "bind";
       };
@@ -14,8 +14,8 @@
       what = "/mnt/eye/documents";
       where = "/home/evanaze/Documents";
       type = "none";
-      wantedBy = [ "syncthing.service" ];
-      before = [ "syncthing.service" ];
+      wantedBy = ["syncthing.service"];
+      before = ["syncthing.service"];
       mountConfig = {
         Options = "bind";
       };
@@ -24,30 +24,8 @@
       what = "/mnt/eye/downloads";
       where = "/home/evanaze/Downloads";
       type = "none";
-      wantedBy = [ "syncthing.service" ];
-      before = [ "syncthing.service" ];
-      mountConfig = {
-        Options = "bind";
-      };
-    }
-  ];
-}
-    {
-      what = "/mnt/eye/documents";
-      where = "/home/evanaze/Documents";
-      type = "none";
-      wantedBy = [ "syncthing.service" ];
-      before = [ "syncthing.service" ];
-      mountConfig = {
-        Options = "bind";
-      };
-    }
-    {
-      what = "/mnt/eye/downloads";
-      where = "/home/evanaze/Downloads";
-      type = "none";
-      wantedBy = [ "syncthing.service" ];
-      before = [ "syncthing.service" ];
+      wantedBy = ["syncthing.service"];
+      before = ["syncthing.service"];
       mountConfig = {
         Options = "bind";
       };
