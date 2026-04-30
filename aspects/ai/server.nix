7825ea737b21ab4580d4d7e48231864d317d0426
Evan Azevedo <me@evanazevedo.com>
2026-04-30 12:32:19 -0600

    fix llama swap syntax

--- aspects/ai/server.nix
+++ aspects/ai/server.nix
@@ -31,7 +31,7 @@ in {
     settings = {
       models = {
         "qwen3.6-35b-a3b" = {
-          cmd = "${llama-server} --port \${PORT} -m /var/lib/llama-cpp/models/Qwen3.6-35B-A3B-UD-Q3_K_S.gguf -ngl 40 -c 8192 -b 512 -t 8 -fa";
+          cmd = "${llama-server} --port \${PORT} -m /var/lib/llama-cpp/models/Qwen3.6-35B-A3B-UD-Q3_K_S.gguf -ngl 40 -c 8192 -b 512 -t 8 -fa on";
           healthCheckTimeout = 180;
         };
       };
