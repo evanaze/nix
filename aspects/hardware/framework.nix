78db60a76a0f6164ec38cf5a12754d267fc0eead
Evan Azevedo <me@evanazevedo.com>
2026-05-01 12:47:12 -0600

    pause prepare sleep

--- aspects/hardware/framework.nix
+++ aspects/hardware/framework.nix
@@ -14,6 +14,8 @@
   # Enable bios updates
   services.fwupd.enable = true;
 
+  hardware.bluetooth.enable = true;
+
   # Enable hardware settings
   hardware.framework = {
     enableKmod = true;
@@ -24,15 +26,15 @@
   };
 
   # Disable wifi and bluetooth before suspend to reduce battery draw in sleep
-  systemd.services.preparesleep = {
-    enable = true;
-    wantedBy = ["sleep.target"];
-    before = ["sleep.target"];
-    description = "Disable wifi and bluetooth before suspend to reduce battery draw in sleep";
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.util-linux}/bin/rfkill block bluetooth wlan";
-      ExecStop = "${pkgs.util-linux}/bin/rfkill unblock bluetooth wlan";
-    };
-  };
+  # systemd.services.preparesleep = {
+  #   enable = true;
+  #   wantedBy = ["sleep.target"];
+  #   before = ["sleep.target"];
+  #   description = "Disable wifi and bluetooth before suspend to reduce battery draw in sleep";
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     ExecStart = "${pkgs.util-linux}/bin/rfkill block bluetooth wlan";
+  #     ExecStop = "${pkgs.util-linux}/bin/rfkill unblock bluetooth wlan";
+  #   };
+  # };
 }
