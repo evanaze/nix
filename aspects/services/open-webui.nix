39d2758d8e31a6d2ff7fe112d86099924ff47bd1
Evan Azevedo <me@evanazevdo.com>
2026-05-03 22:50:03 -0600

    fix error

--- aspects/services/open-webui.nix
+++ aspects/services/open-webui.nix
@@ -3,7 +3,7 @@
     enable = true;
     port = 8725;
     environment = {
-      ENABLE_SIGNUP = false;
+      ENABLE_SIGNUP = "False";
       OPENAI_API_BASE_URL = "http://ai.spitz-pickerel.ts.net:${toString config.services.llama-swap.port}/v1";
     };
   };
