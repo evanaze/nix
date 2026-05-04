05602a3c32cb9a8f6addf70278e88296271f55a9
Evan Azevedo <me@evanazevdo.com>
2026-05-03 22:55:20 -0600

    adding tsserve for openwebui

--- aspects/services/open-webui.nix
+++ aspects/services/open-webui.nix
@@ -1,4 +1,9 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   services.open-webui = {
     enable = true;
     port = 8725;
@@ -6,5 +11,23 @@
       ENABLE_SIGNUP = "False";
       OPENAI_API_BASE_URL = "http://ai.spitz-pickerel.ts.net:${toString config.services.llama-swap.port}/v1";
     };
+  };
+
+  systemd.services.ai-tsserve = {
+    after = [
+      "tailscaled-autoconnect.service"
+      "open-webui.service"
+    ];
+    wants = [
+      "tailscaled-autoconnect.service"
+      "open-webui.service"
+    ];
+    wantedBy = ["multi-user.target"];
+    description = "Using Tailscale Serve to publish Open WebUI";
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    script = "${lib.getExe pkgs.tailscale} serve --service=svc:ai --https=${toString config.services.open-webui.port} ${toString config.services.open-webui.port}";
   };
 }
