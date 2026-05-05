df19e001227f919d056ddd1099716c627062aa5b
Evan Azevedo <me@evanazevedo.com>
2026-05-05 15:46:48 -0600

    jellyfin client

--- aspects/desktop/default.nix
+++ aspects/desktop/default.nix
@@ -1,12 +1,44 @@
 # aspects/desktop/default.nix - Desktop configuration aggregator
-{...}: {
+{
+  pkgs,
+  username,
+  ...
+}: {
   imports = [
     ./apps.nix
     ./email.nix
     ./fonts.nix
     ./gnome.nix
+    ./jellyfin-client.nix
     ./printing.nix
     ./sound.nix
     ./xserver.nix
   ];
+
+  environment.systemPackages = with pkgs; [
+    bitwarden-desktop
+    brave
+    # calibre
+    code-cursor
+    google-chrome
+    hunspell
+    hunspellDicts.en-us
+    inkscape
+    jellyfin-desktop
+    keymapp
+    libreoffice-fresh
+    picard
+    python314
+    slack
+    xclip
+    zoom-us
+  ];
+
+  # Home-manager desktop apps
+  home-manager.users.${username} = {pkgs, ...}: {
+    home.packages = with pkgs; [
+      firefox
+      obsidian
+    ];
+  };
 }
