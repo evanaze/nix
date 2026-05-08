dcbdcb46a35dff42bca50e641b7863e22cfe6df8
Evan Azevedo <me@evanazevdo.com>
2026-05-08 15:52:12 -0600

    trying diff ts command

--- aspects/services/librechat.nix
+++ aspects/services/librechat.nix
@@ -35,6 +35,6 @@
       Type = "oneshot";
       RemainAfterExit = true;
     };
-    script = "${lib.getExe pkgs.tailscale} serve --service=svc:ai --https=4434 ${toString config.services.librechat.env.PORT}";
+    script = "${lib.getExe pkgs.tailscale} serve --service=svc:ai --https=4434 http://localhost:${toString config.services.librechat.env.PORT}";
   };
 }
