7589d20185d8b1d3089cc7062a36d0261223aa81
Evan Azevedo <me@evanazevdo.com>
2026-05-13 11:52:47 -0600

    revert jellyfin

--- aspects/media/jellyfin.nix
+++ aspects/media/jellyfin.nix
@@ -2,79 +2,35 @@
 {
   lib,
   pkgs,
-  config,
+  username,
   ...
 }: {
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
-      rocmPackages.clr.icd # OpenCL for AMD
+      rocmPackages.clr.icd
       libva-utils
       libva-vdpau-driver
       libvdpau-va-gl
     ];
   };
 
-  nixflix = {
+  environment.systemPackages = with pkgs; [
+    jellyfin
+    jellyfin-web
+    jellyfin-ffmpeg
+  ];
+
+  services.jellyfin = {
     enable = true;
-    mediaDir = "/mnt/eye/media";
-    caddy.enable = true;
-    postgres.enable = true;
-    sonarr = {
-      enable = true;
-      config = {
-        apiKey = {
-          _secret = config.sops.secrets."sonarr/api_key".path;
-        };
-        hostConfig.password = {
-          _secret = config.sops.secrets."sonarr/password".path;
-        };
-      };
-    };
-
-    radarr = {
-      enable = true;
-      config = {
-        apiKey = {
-          _secret = config.sops.secrets."radarr/api_key".path;
-        };
-        hostConfig.password = {
-          _secret = config.sops.secrets."radarr/password".path;
-        };
-      };
-    };
-
-    prowlarr = {
-      enable = true;
-      config = {
-        apiKey = {
-          _secret = config.sops.secrets."prowlarr/api_key".path;
-        };
-        hostConfig.password = {
-          _secret = config.sops.secrets."prowlarr/password".path;
-        };
-      };
-    };
-
-    sabnzbd = {
-      enable = true;
-      settings = {
-        misc.api_key = {
-          _secret = config.sops.secrets."sabnzbd/api_key".path;
-        };
-      };
-    };
-
-    jellyfin = {
-      enable = true;
-      users.admin = {
-        policy.isAdministrator = true;
-        password = {
-          _secret = config.sops.secrets."jellyfin/admin_password".path;
-        };
-      };
-    };
+    user = username;
+    dataDir = "/var/lib/jellyfin";
   };
+
+  users.users.${username}.extraGroups = [
+    "video"
+    "render"
+  ];
 
   systemd.services.jellyfin-tsserve = {
     after = [
