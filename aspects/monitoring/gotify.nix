dd6b86cd99f6110a48f8dbe84a1d4f481d0cc375
Evan Azevedo <me@evanazevdo.com>
2026-05-13 09:15:39 -0600

    gotify

--- aspects/monitoring/gotify.nix
+++ aspects/monitoring/gotify.nix
@@ -1,13 +1,14 @@
 {...}: {
   services.gotify = {
     enable = true;
-  };
-
-  services.prometheus.alertmanagerGotify = {
-    enable = true;
     environment = {
       GOTIFY_SERVER_PORT = 8117;
       GOTIFY_DATABASE_DIALECT = "sqlite3";
     };
+  };
+
+  services.prometheus.alertmanagerGotify = {
+    enable = true;
+    metrics.username = "admin";
   };
 }
