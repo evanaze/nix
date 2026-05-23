f0d5f0a5ca069dcd20533e74fac930173df8ac78
Evan Azevedo <me@evanazevdo.com>
2026-05-23 11:02:44 -0600

    fix opencode bug

--- aspects/development/opencode.nix
+++ aspects/development/opencode.nix
@@ -1,23 +1,29 @@
-{config, ...}: {
-  opencode = {
-    enable = true;
-    tui.theme = "catppuccin";
-    settings = {
-      autoupdate = true;
-      lsp = true;
-      provider = {
-        llama-local = {
-          name = "Llama Swap";
-          npm = "@ai-sdk/openai-compatible";
-          options = {
-            baseURL = "https://llm.spitz-pickerel.ts.net:${toString config.services.llama-swap.port}/v1";
-          };
-          models = {
-            "gemma-4-e4b-q8" = {
-              name = "gemma-4-e4b-q8";
+{
+  username,
+  config,
+  ...
+}: {
+  home-manager.users.${username}.programs = {
+    opencode = {
+      enable = true;
+      tui.theme = "catppuccin";
+      settings = {
+        autoupdate = true;
+        lsp = true;
+        provider = {
+          llama-local = {
+            name = "Llama Swap";
+            npm = "@ai-sdk/openai-compatible";
+            options = {
+              baseURL = "https://llm.spitz-pickerel.ts.net:${toString config.services.llama-swap.port}/v1";
             };
-            "qwen3.6-35b-a3b" = {
-              name = "qwen3.6-35b-a3b";
+            models = {
+              "gemma-4-e4b-q8" = {
+                name = "gemma-4-e4b-q8";
+              };
+              "qwen3.6-35b-a3b" = {
+                name = "qwen3.6-35b-a3b";
+              };
             };
           };
         };
