27d42d3c970d1616d67f3aa775593b2d608434a2
Evan Azevedo <me@evanazevdo.com>
2026-05-24 18:11:14 -0600

    setting up models for openviking

--- aspects/services/openviking.nix
+++ aspects/services/openviking.nix
@@ -1,20 +1,32 @@
-{...}: {
+{
+  pkgs,
+  config,
+  inputs,
+  ...
+}: {
+  nixpkgs.overlays = [inputs.openviking.overlays.default];
+
   services.openviking = {
     enable = true;
     readOnlyPaths = [];
     settings = {
       embedding.dense = {
         provider = "openai";
-        model = "text-embedding-004";
-        api_key = "your-api-key"; # Use sops-nix for security
-        api_base = "https://generativelanguage.googleapis.com/v1beta/openai/";
+        model = "nvidia/llama-nemotron-embed-vl-1b-v2:free";
+        api_key = config.sops.secrets.openrouter-api-key;
+        api_base = "https://openrouter.ai/api/v1/chat/completions";
         dimension = 768;
       };
       vlm = {
-        provider = "litellm";
-        model = "gemini/gemini-2.0-flash";
-        api_key = "your-api-key";
+        provider = "openai";
+        model = "google/gemini-embedding-2-preview";
+        api_key = config.sops.secrets.openrouter-api-key;
+        api_base = "https://openrouter.ai/api/v1/chat/completions";
       };
     };
   };
+
+  environment.systemPackages = with pkgs; [
+    openviking
+  ];
 }
