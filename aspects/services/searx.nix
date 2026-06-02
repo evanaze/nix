93df0233f45c92866c799bae58639db14a05cb4f
Evan Azevedo <me@evanazevdo.com>
2026-06-02 10:43:51 -0600

    fixing searxng

--- aspects/services/searx.nix
+++ aspects/services/searx.nix
@@ -3,7 +3,7 @@
 
   services.searx = {
     enable = true;
-    environmentFile = config.sops.secrets."searxng-env".path;
+    environmentFile = config.sops.secrets."searxng/env".path;
     settings.server = {
       bind_address = "::1";
     };
