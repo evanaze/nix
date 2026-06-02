7adc0cd84c325b33b81ab1f18dedfb01d95b3f54
Evan Azevedo <me@evanazevedo.com>
2026-06-02 10:52:24 -0600

    use gnome instead of illuminanced

--- aspects/hardware/mars/auto-brightness.nix
+++ aspects/hardware/mars/auto-brightness.nix
@@ -5,18 +5,18 @@
 }: {
   environment.systemPackages = with pkgs; [
     gnomeExtensions.adaptive-brightness
-    illuminanced
+    # illuminanced
   ];
 
-  systemd.services.illuminanced = {
-    wantedBy = ["multi-user.target"];
-    description = "Service to set screen brightness automatically";
-    serviceConfig = {
-      Type = "forking";
-      Restart = "on-failure";
-      PIDFile = "/run/illuminanced.pid";
-    };
+  # systemd.services.illuminanced = {
+  #   wantedBy = ["multi-user.target"];
+  #   description = "Service to set screen brightness automatically";
+  #   serviceConfig = {
+  #     Type = "forking";
+  #     Restart = "on-failure";
+  #     PIDFile = "/run/illuminanced.pid";
+  #   };
 
-    script = "${lib.getExe pkgs.illuminanced} -c ${./illuminanced.toml}";
-  };
+  #   script = "${lib.getExe pkgs.illuminanced} -c ${./illuminanced.toml}";
+  # };
 }
