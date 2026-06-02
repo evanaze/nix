15dd5bf2994c9764652e661f7ab9ec41cf64a488
Evan Azevedo <me@evanazevdo.com>
2026-06-02 11:00:02 -0600

    setting up searxng in hermes

--- aspects/services/searx.nix
+++ aspects/services/searx.nix
@@ -1,4 +1,9 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   sops.secrets."searxng/env" = {};
 
   services.searx = {
@@ -7,5 +12,23 @@
     settings.server = {
       bind_address = "::1";
     };
+  };
+
+  systemd.services.searxng-tsserve = {
+    after = [
+      "tailscaled-autoconnect.service"
+      "searx.service"
+    ];
+    wants = [
+      "tailscaled-autoconnect.service"
+      "searx.service"
+    ];
+    wantedBy = ["multi-user.target"];
+    description = "Using Tailscale Serve to publish Searxng";
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    script = "${lib.getExe pkgs.tailscale} serve --service=svc:search --https=4441 8312";
   };
 }
