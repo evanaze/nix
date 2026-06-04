32300ffec3a287f5eff9e171d417a83023bba1f8
Evan Azevedo <me@evanazevedo.com>
2026-06-03 23:34:13 -0600

    slippi

--- aspects/gaming/slippi.nix
+++ aspects/gaming/slippi.nix
@@ -1,5 +1,13 @@
 # aspects/gaming/slippi.nix - Super Smash Bros Melee Slippi setup
-{username, ...}: {
+{
+  username,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [
+    usbutils
+  ];
+
   home-manager.users.${username} = {
     slippi-launcher = {
       enable = true;
@@ -11,8 +19,15 @@
 
   hardware.graphics.enable = true;
 
-  # GameCube controller adapter udev rules
+  # Prevent hid-nintendo and hid_mf (Mayflash force-feedback driver) from
+  # claiming the GameCube adapter (needs usbhid for Dolphin/Slippi)
+  boot.blacklistedKernelModules = ["hid_nintendo" "hid_mf"];
+
+  # GameCube controller adapter udev rules (Nintendo + DragonRise)
   services.udev.extraRules = ''
     SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0337", MODE="0666"
+    SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="0079", ATTRS{idProduct}=="1846", MODE="0666"
+    SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="0079", ATTRS{idProduct}=="1843", MODE="0666"
+    SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="0079", ATTRS{idProduct}=="1844", MODE="0666"
   '';
 }
