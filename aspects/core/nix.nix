f1d23611341d7fad2496b637479b4feb4ef2390a
Evan Azevedo <me@evanazevedo.com>
2026-06-05 22:36:14 -0600

    adding cache stuff

--- aspects/core/nix.nix
+++ aspects/core/nix.nix
@@ -1,7 +1,17 @@
-# aspects/core/nix.nix - Nix settings, flakes, unfree packages
-{username, ...}: {
+# aspects/core/nix.nix - Nix settings, flakes, unfree packages, cache pushing
+{config, pkgs, lib, username, ...}: let
+  cacheHost = "evanaze@jupiter.spitz-pickerel.ts.net";
+in {
   nix = {
-    extraOptions = "trusted-users = root ${username}";
+    extraOptions = ''
+      trusted-users = root ${username}
+      post-build-hook = ${pkgs.writeShellScript "nix-post-build-hook" ''
+        set -eu
+        for p in "$@"; do
+          exec sudo -u ${username} nix copy --to ssh://${cacheHost} "$p"
+        done
+      ''}
+    '';
     settings.experimental-features = [
       "nix-command"
       "flakes"
