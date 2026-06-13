8143cb222967343c33e345b91e4e460dfb34c228
Evan Azevedo <me@evanazevedo.com>
2026-06-13 09:08:52 -0600

    fixing laptop audio

--- aspects/hardware/mars/framework.nix
+++ aspects/hardware/mars/framework.nix
@@ -24,4 +24,35 @@
       hideRawDevice = true;
     };
   };
+
+  # The audioEnhancement virtual sink defaults to 34.3% volume.
+  # This service sets it to 100% after pipewire starts.
+  systemd.user.services.framework-speakers-volume = {
+    enable = true;
+    description = "Set Framework Speakers volume to 100%";
+    after = ["pipewire.service"];
+    bindsTo = ["pipewire.service"];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = let
+        script = pkgs.writeShellScript "set-framework-volume" ''
+          for i in $(seq 1 10); do
+            id=$(${pkgs.pipewire}/bin/pw-cli list-objects 2>/dev/null \
+              | ${pkgs.gnugrep}/bin/grep -B1 'node.name = "audio_effect.laptop-convolver"' \
+              | ${pkgs.gnused}/bin/sed -n 's/.*id \([0-9]*\).*/\1/p')
+            if [ -n "$id" ]; then
+              ${pkgs.pipewire}/bin/wpctl set-volume "$id" 1.0
+              exit 0
+            fi
+            sleep 0.5
+          done
+          exit 1
+        '';
+      in "${script}";
+      Restart = "on-failure";
+      RestartSec = 3;
+    };
+    wantedBy = ["pipewire.service"];
+  };
 }
