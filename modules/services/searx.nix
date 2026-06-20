f417b90d5b20200355c51d5c835da2a053dcc818
Evan Azevedo <me@evanazevedo.com>
2026-06-19 21:51:24 -0600

    allow searx tool calls

--- modules/services/searx.nix
+++ modules/services/searx.nix
@@ -1,59 +1,63 @@
 let
   module = {
-  config,
-  lib,
-  pkgs,
-  ...
-}: let
-  searxngPort = 8311;
-  caddyPort = 8312;
-in {
-  sops.secrets."searxng/env" = {};
+    config,
+    lib,
+    pkgs,
+    ...
+  }: let
+    searxngPort = 8311;
+    caddyPort = 8312;
+  in {
+    sops.secrets."searxng/env" = {};
 
-  services.searx = {
-    enable = true;
-    environmentFile = config.sops.secrets."searxng/env".path;
-    settings.server = {
-      bind_address = "127.0.0.1";
-      port = searxngPort;
+    services.searx = {
+      enable = true;
+      environmentFile = config.sops.secrets."searxng/env".path;
+      settings.server = {
+        bind_address = "127.0.0.1";
+        port = searxngPort;
+      };
+      search.formats = [
+        "html"
+        "json"
+      ];
+    };
+
+    services.caddy.virtualHosts."http://:${toString caddyPort}" = {
+      extraConfig = ''
+        reverse_proxy localhost:${toString searxngPort} {
+          header_up X-Forwarded-Proto https
+          header_up X-Forwarded-For {remote_host}
+          header_up X-Forwarded-Host {host}
+        }
+      '';
+    };
+
+    systemd.services.searxng-tsserve = {
+      after = [
+        "tailscaled-autoconnect.service"
+        "tailscaled.service"
+        "searx.service"
+      ];
+      wants = [
+        "tailscaled-autoconnect.service"
+        "tailscaled.service"
+        "searx.service"
+      ];
+      wantedBy = ["multi-user.target"];
+      description = "Using Tailscale Serve to publish Searxng";
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        Restart = "on-failure";
+        RestartSec = "10s";
+      };
+      script = ''
+        ${lib.getExe pkgs.tailscale} serve clear svc:search || true
+        ${lib.getExe pkgs.tailscale} serve --service=svc:search --https=443 http://127.0.0.1:${toString caddyPort}
+      '';
     };
   };
-
-  services.caddy.virtualHosts."http://:${toString caddyPort}" = {
-    extraConfig = ''
-      reverse_proxy localhost:${toString searxngPort} {
-        header_up X-Forwarded-Proto https
-        header_up X-Forwarded-For {remote_host}
-        header_up X-Forwarded-Host {host}
-      }
-    '';
-  };
-
-  systemd.services.searxng-tsserve = {
-    after = [
-      "tailscaled-autoconnect.service"
-      "tailscaled.service"
-      "searx.service"
-    ];
-    wants = [
-      "tailscaled-autoconnect.service"
-      "tailscaled.service"
-      "searx.service"
-    ];
-    wantedBy = ["multi-user.target"];
-    description = "Using Tailscale Serve to publish Searxng";
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      Restart = "on-failure";
-      RestartSec = "10s";
-    };
-    script = ''
-      ${lib.getExe pkgs.tailscale} serve clear svc:search || true
-      ${lib.getExe pkgs.tailscale} serve --service=svc:search --https=443 http://127.0.0.1:${toString caddyPort}
-    '';
-  };
-};
 in {
   flake.modules.nixos = {
     servicesSearx = module;
