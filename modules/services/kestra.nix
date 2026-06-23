81dc8cc9ca5610c43c8732305a11cfd25ff6c288
Evan Azevedo <me@evanazevdo.com>
2026-06-23 14:05:03 -0600

    tsserver kestra

--- modules/services/kestra.nix
+++ modules/services/kestra.nix
@@ -3,6 +3,7 @@ let
     config,
     inputs,
     pkgs,
+    lib,
     ...
   }: {
     nixpkgs.overlays = [inputs.kestra-nix.overlays.default];
@@ -30,6 +31,29 @@ let
       };
       encryptionSecretKeyFile = config.sops.secrets."kestra/encryption-secret-key".path;
       jdbcSecretKeyFile = config.sops.secrets."kestra/jdbc-secret-key".path;
+    };
+
+    systemd.services.kestra-tsserve = {
+      after = [
+        "tailscaled-autoconnect.service"
+        "kestra.service"
+      ];
+      wants = [
+        "tailscaled-autoconnect.service"
+        "kestra.service"
+      ];
+      wantedBy = ["multi-user.target"];
+      description = "Using Tailscale Serve to publish Kestra";
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        Restart = "on-failure";
+        RestartSec = "10s";
+      };
+      script = ''
+        ${lib.getExe pkgs.tailscale} serve clear svc:jobs || true
+        ${lib.getExe pkgs.tailscale} serve --service=svc:jobs --https=443 ${config.services.kestra.port}
+      '';
     };
   };
 in {
