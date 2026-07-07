643ade77eb6eb3b3c650258f6af74ed73faf49f9
Evan Azevedo <me@evanazevdo.com>
2026-07-07 17:43:11 -0600

    adding accountability skill

--- modules/services/hermes-agent.nix
+++ modules/services/hermes-agent.nix
@@ -50,7 +50,7 @@ let
       repo = "stackmagic-accountability";
       rev = "refs/heads/main";
       private = true;
-      hash = lib.fakeHash;
+      hash = "sha256-TVC0AktR4WhiSoZ7Zb2nQfZ0+F+5ZOX3ZkN4vOJqR80=";
     };
 
     common-hermes-settings = {
