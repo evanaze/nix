6c11ebdc69641dff1f5ea2d6fd97ed482d40165d
Evan Azevedo <me@evanazevdo.com>
2026-07-26 12:08:44 -0600

    trying nix stable

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
     home-manager = {
