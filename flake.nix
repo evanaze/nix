0784c1643711ef99ad3b728d86732ff20c910c06
Evan Azevedo <me@evanazevdo.com>
2026-07-27 11:00:30 -0600

    pinning hm

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     kestra-nix = {
