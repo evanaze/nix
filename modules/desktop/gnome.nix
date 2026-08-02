1032808e4bd32fbfd8fcf73f4fc4a68c385fd161
Evan Azevedo <me@evanazevedo.com>
2026-08-02 14:24:09 -0600

    fixing caffeine with gsettings

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -5,7 +5,24 @@ let
       pkgs,
       username,
       ...
-    }: {
+    }:
+    let
+      # Caffeine ships its GSettings schema only inside the extension dir
+      # (…/caffeine@patapon.info/schemas/), which glib's global lookup on
+      # XDG_DATA_DIRS never sees. SymlinkJoin it into the standard
+      # glib-2.0/schemas location so standalone `gsettings` can find it.
+      caffeineWrapped = pkgs.symlinkJoin {
+        name = "caffeine-gnome-extension";
+        paths = [pkgs.gnomeExtensions.caffeine];
+        postBuild = ''
+          mkdir -p $out/share/glib-2.0/schemas
+          ln -s \
+            $out/share/gnome-shell/extensions/caffeine@patapon.info/schemas/gschemas.compiled \
+            $out/share/glib-2.0/schemas/gschemas.compiled
+        '';
+      };
+    in {
+
       services.desktopManager.gnome.enable = true;
 
       services.displayManager = {
@@ -16,8 +33,8 @@ let
         gdm.enable = true;
       };
 
-      environment.systemPackages = with pkgs; [
-        gnomeExtensions.caffeine
+      environment.systemPackages = [
+        caffeineWrapped
       ];
 
       # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
