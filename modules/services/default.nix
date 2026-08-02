0ab0a930b9232cd25e054f61b6c8e8ce9b5e91fc
Evan Azevedo <me@evanazevdo.com>
2026-08-02 08:56:37 -0600

    adding tag to home server

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -1,5 +1,8 @@
 {
   flake.modules.nixos.services = {
+    services.tailscale.extraUpFlags = [
+      "--advertise-tags=tag:home-server"
+    ];
     services.tailscale.extraSetFlags = [
       "--advertise-exit-node"
       "--accept-dns=false"
