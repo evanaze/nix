c72f4ae1022f6cc6b20f81f944dfe3d146ce8d55
Evan Azevedo <me@evanazevdo.com>
2026-08-14 00:37:57 -0600

    auto: update flake inputs

--- pkgs/stackmagic-research/default.nix
+++ pkgs/stackmagic-research/default.nix
@@ -21,7 +21,7 @@ buildPythonPackage {
     repo = "stackmagic-research";
     rev = "86054c6892698cba7e9ff8db10bdf5e0f79d0bce";
     private = true;
-    hash = lib.fakeHash;
+    hash = "sha256-HutfzrEK4yr6PZhD42EHxGvQHFk290DqRF+ziYKAIx8=";
   };
 
   nativeBuildInputs = [
