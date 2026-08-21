e13a9bf850ad1320fcae058b6fadaceec9c5185b
Evan Azevedo <me@evanazevdo.com>
2026-08-21 10:57:05 -0600

    Revert "auto: update flake inputs"
    This reverts commit 1d7082a44603c660862401134b9a69719c76d3fe.

--- modules/services/paseo.nix
+++ modules/services/paseo.nix
@@ -28,7 +28,7 @@ let
       '';
     };
 
-    services.tailscale.serve.services.paseo.endpoints."tcp:443" = "http://127.0.0.1:${toString caddyPort}";
+    services.tailscale.serve.services.paseo.endpoints."tcp:443" = "https://127.0.0.1:${toString caddyPort}";
   };
 in {
   flake.modules.nixos = {
